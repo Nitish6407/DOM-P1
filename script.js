ca4fff2cb083bbc3d5e154b933b0b05b7d555b13
Nitish6407 <contactnitish407@gmail.com>
2024-07-30 21:36:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Fetch the div element by its ID
+var myDiv = document.getElementById('myDiv');
+
+// Change the background color to yellow
+myDiv.style.backgroundColor = 'yellow';
+
+// Add a margin of 20 pixels
+myDiv.style.margin = '20px';
+
+// Apply a padding of 10 pixels
+myDiv.style.padding = '10px';
+
+// Change the font size to 18 pixels
+myDiv.style.fontSize = '18px';
+
+// Set the font weight to bold
+myDiv.style.fontWeight = 'bold';
+
+// Change the height to 200 pixels
+myDiv.style.height = '200px';
+
+// Modify the width to 300 pixels
+myDiv.style.width = '300px';
